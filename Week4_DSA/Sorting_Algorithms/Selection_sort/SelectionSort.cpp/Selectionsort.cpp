0f3be690ebdd8da923c7811b34e261be179a0e78
Deepanshu <deepanshurathore975@gmail.com>
2023-10-12 18:55:23 +0530

    Today i learn the sorting algo selection sort

---
+++ Week4_DSA/Sorting_Algorithms/Selection_sort/SelectionSort.cpp/Selectionsort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int arr[1000];
+  int n;
+  cout<<"Enter a number u want the array of : ";
+  cin>>n;
+
+  cout<<"Enter the element's in array : ";
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+
+  // Selction sort  in ascending order 
+  for(int i=0;i<n-1;i++){
+    int index =i;
+    for(int j=i+1;j<n;j++){
+      if(arr[j] < arr[index])
+      index =j;
+    }
+
+    swap(arr[i],arr[index]);
+  }
+
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+
+  return 0;
+}
