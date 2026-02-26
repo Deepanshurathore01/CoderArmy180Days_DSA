253a55060236c5b7f3a2eceffa225a445be06b23
Deepanshu <deepanshurathore975@gmail.com>
2026-02-26 18:11:58 +0530

    main ompletd

--- GRAPH_DS/Implementation/Undirected Graph/Grapimp.cpp
+++ GRAPH_DS/Implementation/Undirected Graph/Grapimp.cpp
@@ -11,18 +11,19 @@ int main()
   int vertex, edges;
   cin>>vertex>>edges;
 // undirected unweighted graph
-  // vector<vector<bool> >AdjMatrix(vertex ,vector<bool>(vertex,0));
+  vector<vector<bool> >AdjMatrix(vertex ,vector<bool>(vertex,0));
 
 // undirected weighted graph
-  vector<vector<int> >AdjMatrix(vertex ,vector<int>(vertex,0));
+  // vector<vector<int> >AdjMatrix(vertex ,vector<int>(vertex,0));
 
-  // int u ,v;
-  int u ,v,weight;
+  int u ,v;
+  // int u ,v,weight;
   for(int i=0;i<edges;i++)
   {
-    cin>>u>>v>>weight;
-    AdjMatrix[u][v]=weight;
-    AdjMatrix[v][u]=weight;
+    cin>>u>>v;
+    // cin>>u>>v>>weight;
+    AdjMatrix[u][v]=1;
+    AdjMatrix[v][u]=1;
   }
 
   for(int i=0;i<vertex;i++)
